97cb09ebb952daab3042e4debfa2fe8547a4e9e6
Sukomal-d <sukomal.it@gmail.com>
2023-10-07 21:44:33 +0530

    adding all

---
+++ function.sh
@@ -0,0 +1,24 @@
+rightfun()
+{
+	for i in {1..10}
+	do 
+		echo "right function is called"
+
+	done
+
+	}
+wrongfun()
+{
+	for i in {1..5}
+	do
+		echo "wrong function called"
+	done
+}
+read -p "enter your name " name
+if [ $name == Sukomal ]
+then
+	rightfun
+else
+	wrongfun
+fi
+
